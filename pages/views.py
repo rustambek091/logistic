4c544a29a36a0b767a733fa02f142eb1a24d95ea
Ismatov Rustam <137518336+rustambek091@users.noreply.github.com>
2023-09-18 17:05:33 +0300

    first commit

---
+++ pages/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView, ListView, DetailView
+
+from .models import * 
+
+# Create your views here.
+
+
+class Home(TemplateView):
+    template_name = 'index.html'
+
+class About(TemplateView):
+    template_name = 'about.html'
+
+class Contact(TemplateView):
+    template_name = 'contact.html'
+
+class Services(Services):
+    template_name = 'Services.html'
+# def Services(request):
+#     servis = Service.objects.all()
+    # context = {
+    #     'servis':servis
+    # }
+    # return render(request,'services.html', context)
