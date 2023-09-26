a6fef18364e52b4103c309ab72ab921573402837
Ismatov Rustam <137518336+rustambek091@users.noreply.github.com>
2023-09-26 16:49:34 +0300

    push

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
+from django.urls import path 
 from .views import * 
 
+
 urlpatterns = [
-    path('', Index.as_view(), name='home'),
+    path('', Home.as_view(), name='home'),
     path('about/', About.as_view(), name='about'),
     path('contact/', Contact.as_view(), name='contact'),
     path('services/', Services.as_view (), name='services'),
